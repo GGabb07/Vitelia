1be8d2bc7f3c48ff5ffdf6546ab6c8892d1c9dce
GGabb07 <gm.gabri.marino@gmail.com>
2026-05-10 11:35:57 +0200

    Modified player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,33 +2,33 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-const SPEED = 100.0
+const SPEED = 80.0
 
 var last_dir := "front"
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	var direction := Vector2(
-		Input.get_axis("left", "right"),
-		Input.get_axis("back", "front")
-	).normalized()
-	if direction.x > 0:
-		last_dir = "right"
-	elif direction.x < 0:
-		last_dir = "left"
-	elif direction.y > 0:
-		last_dir = "front"
-	elif direction.y < 0:
-		last_dir = "back"
-	
-	if direction:
-		velocity = direction * SPEED
-		animation_player.play("move_" + last_dir)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		animation_player.play("idle_" + last_dir)
+	move()
 	move_and_slide()
+
+func move():
+	var input := Input.get_axis("back", "front")
+	if input > 0:
+		velocity = Vector2.DOWN * SPEED
+		last_dir = "front"
+	elif input < 0:
+		velocity = Vector2.UP * SPEED
+		last_dir = "back"
+	else:
+		input = Input.get_axis("left", "right")
+		if input > 0:
+			velocity = Vector2.RIGHT * SPEED
+			last_dir = "right"
+		elif input < 0:
+			velocity = Vector2.LEFT * SPEED
+			last_dir = "left"
+		else:
+			animation_player.play("idle_" + last_dir)
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+			return
+	animation_player.play("move_" + last_dir)
